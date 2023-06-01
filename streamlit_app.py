e37678d15f417701ae05e858e7812d3cccfa153c
avinashaltimetrik <135209274+avinashaltimetrik@users.noreply.github.com>
2023-06-02 00:09:42 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title("My parents new healthy diner")
 streamlit.header("Breakfast menu")
-streamlit.title("Omega 3")
-streamlit.title("Kale")
-streamlit.title("Hard-Boiled")
+streamlit.text("Omega 3")
+streamlit.text("Kale")
+streamlit.text("Hard-Boiled")
