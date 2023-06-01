bee44ce761fd4df312c85e729f93cdbda9a2c5fb
avinashaltimetrik <135209274+avinashaltimetrik@users.noreply.github.com>
2023-06-02 00:09:10 +0530

    Update streamlit_app.py
    Added content

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("My parents new healthy diner")
+streamlit.header("Breakfast menu")
+streamlit.title("Omega 3")
+streamlit.title("Kale")
+streamlit.title("Hard-Boiled")
