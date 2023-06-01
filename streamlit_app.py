df16cc17f0f7501610d31f5e59139a2112794ccc
avinashaltimetrik <135209274+avinashaltimetrik@users.noreply.github.com>
2023-06-02 00:12:23 +0530

    Update streamlit_app.py
    Emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title("My parents new healthy diner")
 streamlit.header("Breakfast menu")
-streamlit.text("Omega 3")
-streamlit.text("Kale")
-streamlit.text("Hard-Boiled")
+streamlit.text("🥣 Omega 3") 
+streamlit.text("🥗 Kale")
+streamlit.text("🐔 Hard-Boiled")
+streamlit.text("🥑 🍞 Avacodo")
